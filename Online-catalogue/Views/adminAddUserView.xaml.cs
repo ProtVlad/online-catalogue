6ed923583b143ea3b9c2a11ca02d47d5162fe366
sgeorgiana27 <147037447+sgeorgiana27@users.noreply.github.com>
2025-03-25 18:14:03 +0200

    Added admin view

---
+++ Online-catalogue/Views/adminAddUserView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Online_catalogue.Views
+{
+    /// <summary>
+    /// Interaction logic for adminAddUserView.xaml
+    /// </summary>
+    public partial class adminAddUserView : Window
+    {
+        public adminAddUserView()
+        {
+            InitializeComponent();
+        }
+
+       
+
+private void SaveButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(IdTextBox.Text) ||
+            string.IsNullOrEmpty(LastNameTextBox.Text) ||
+            string.IsNullOrEmpty(FirstNameTextBox.Text) ||
+             RoleComboBox.SelectedItem == null ||
+            string.IsNullOrEmpty(EmailTextBox.Text))
+        {
+            MessageBox.Show("Toate câmpurile trebuie completate!", "Eroare", MessageBoxButton.OK, MessageBoxImage.Error);
+            return; 
+        }
+
+        // Validare email
+        string email = EmailTextBox.Text;
+        string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"; 
+        if (!Regex.IsMatch(email, emailPattern))
+        {
+            MessageBox.Show("Adresa de email nu este validă!", "Eroare", MessageBoxButton.OK, MessageBoxImage.Error);
+            return; 
+        }
+
+        // Dacă totul este în regulă, închide fereastra
+        this.Close();
+    }
+
+
+}
+}
---
+++ Online-catalogue/Views/adminView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Online_catalogue.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Online_catalogue.Views
+{
+    /// <summary>
+    /// Interaction logic for adminView.xaml
+    /// </summary>
+    public partial class adminView : Window
+    {
+        public adminView()
+        {
+            InitializeComponent();
+        }
+
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+             adminAddUserView adminAddUserView = new adminAddUserView();
+            adminAddUserView.ShowDialog();
+        }
+
+        private void EditUser_Click(object sender, RoutedEventArgs e)
+        {
+            //if (UsersDataGrid.SelectedItem is User selectedUser)
+            //{
+            //    EditUserWindow editWindow = new EditUserWindow(selectedUser);
+            //    editWindow.ShowDialog();
+
+            //    LoadUsers();
+            //}
+
+            //cred ca putem folosi si adminAddUserView
+        }
+
+        private void DeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            //if (UsersDataGrid.SelectedItem is User selectedUser)
+            //{
+            //    MessageBoxResult result = MessageBox.Show($"Sigur vrei să ștergi utilizatorul {selectedUser.LastName}?",
+            //                                              "Confirmare ștergere",
+            //                                              MessageBoxButton.YesNo,
+            //                                              MessageBoxImage.Warning);
+
+            //    if (result == MessageBoxResult.Yes)
+            //    {
+            //        DeleteUser(selectedUser.Id);
+
+            //        LoadUsers();
+            //    }
+            //}
+        }
+
+        // Metoda care șterge utilizatorul din baza de date
+        private void DeleteUser(int userId)
+        {
+           // Users.Remove(Users.FirstOrDefault(u => u.Id == userId));
+        }
+
+
+    }
+}
